9731a71d2c598aa650e81ad6b1ef2a9aa37b0d36
Avi P <avip921@gmail.com>
2025-04-16 19:25:13 -0500

    Finished HW 11

--- HW 11 - Temperature Converter/convert.js
+++ HW 11 - Temperature Converter/convert.js
@@ -5,22 +5,46 @@ function domLoaded() {
     document.getElementById("convertButton").addEventListener("click", () => {
         const celsius = document.getElementById("cInput").value;
         const fahrenheit = document.getElementById("fInput").value;
+        const error = document.getElementById("errorMessage");
+        const weatherImage = document.getElementById("weatherImage");
 
-        if (celsius !== ""){
+        let celsiusTemp;
+
+        if (isNaN(celsius) || isNaN(fahrenheit)) {
+            error.innerHTML = (celsius + fahrenheit + " is not a number");
+        }
+        else if (celsius !== ""){
             document.getElementById("fInput").value = convertCtoF(parseFloat(celsius));
+            celsiusTemp = parseFloat(celsius);
         }
         else if (fahrenheit !== "") {
-            document.getElementById("cInput").value = convertFtoC(parseFloat(fahrenheit));
+            celsiusTemp = convertFtoC(parseFloat(fahrenheit));
+            document.getElementById("cInput").value = celsiusTemp;
         }
+
+        if (celsiusTemp < 32) {
+            weatherImage.src = "images/cold.png";
+        }
+        else if (celsiusTemp >= 32 && celsiusTemp <= 50) {
+            weatherImage.src = "images/cool.png";
+        }
+        else {
+            weatherImage.src = "images/warm.png";
+            weatherImage.alt = "Warm";
+        }
+
     });
 
     document.getElementById("cInput").addEventListener("input", () => { { /*https://developer.mozilla.org/en-US/docs/Web/API/Element/input_event */}
         document.getElementById("fInput").value = "";
+        document.getElementById("errorMessage").innerHTML = "";
     });
 
     document.getElementById("fInput").addEventListener("input", () => {
         document.getElementById("cInput").value = "";
+        document.getElementById("errorMessage").innerHTML = "";
     });
+
 }
 
 function convertCtoF(degreesCelsius) {
